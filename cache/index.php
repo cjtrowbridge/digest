48f0f557e65c181519f942bc9d1bcca1b7dc74bd
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-09-04 00:40:41 -0700

    Update index.php

--- cache/index.php
+++ cache/index.php
@@ -11,3 +11,14 @@ if(!(file_exists('whitelist.php'))){
   $Key = sha256(uniqid('',true));
   file_put_contents('whitelist.php','<?php $Key = "'.$Key.'";'.PHP_EOL.'$Whitelist=array();'.PHP_EOL.PHP_EOL);
 }
+
+include('whitelist.php');
+
+if(
+  isset($_REQUEST['key']) &&
+  $_REQUEST['key'] == $Key
+){
+  if(isset($_REQUEST['add'])){
+    file_put_contents('whitelist.php','$Whitelist[]="'.$_REQUEST['add'].'";'.PHP_EOL,FILE_APPEND);
+  }
+}
