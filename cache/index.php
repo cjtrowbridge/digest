682daa0d600328cdcae7733970d452a7568c96c9
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-09-04 00:47:42 -0700

    Update index.php

--- cache/index.php
+++ cache/index.php
@@ -49,7 +49,7 @@ if(
           
           <div class="form-group">
             <label for="key">Key</label>
-            <input type="text" class="form-control" id="key" name="key" <?php if(isset($_REQUEST['key'])){ echo ' value="'.$_REQUEST['key'].'";} ?> >
+            <input type="text" class="form-control" id="key" name="key" <?php if(isset($_REQUEST['key'])){ echo ' value="'.$_REQUEST['key'].'"';} ?> >
           </div>
           
           <div class="form-group">
