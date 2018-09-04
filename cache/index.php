d0c3443cc0fba3007756e6448333eb487417f037
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-09-04 00:33:26 -0700

    Create index.php

---
+++ cache/index.php
@@ -0,0 +1,13 @@
+<?php
+
+//This script receives urls and returns the contents if the url is found in the whitelist file.
+
+function sha256($Input){
+  return hash('sha256',$Input);
+}
+
+//Make sure there is a whitelist file
+if(!(file_exists('whitelist.php'))){
+  $Key = sha256(uniqid(true));
+  file_put_contents('whitelist.php','<?php $Key = "";'.PHP_EOL.'$Whitelist=array();'.PHP_EOL.PHP_EOL);
+}
